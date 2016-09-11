d89b5f76718dd288078c1ad2d0e42011f9a0ee82
Shidfar Hodizoda <shidfarhf@yahoo.com>
2016-09-11 21:09:21 +0800

    added testing thread

--- ServerEngine.cpp
+++ ServerEngine.cpp
@@ -5,10 +5,15 @@
 #include "ServerEngine.h"
 
 int mqttCallback(const mosquitto_message* message, void* context);
+void *tester(void* context);
 
 ServerEngine::ServerEngine()
 {
     this->mqttClient = new MqttClient();
+    this->mqttClient->SetCallback(mqttCallback, this);
+
+    pthread_t test_thread;
+    int err = pthread_create(&test_thread, NULL, tester, this->mqttClient);
 }
 
 int mqttCallback(const mosquitto_message* message, void* context)
@@ -18,4 +23,22 @@ int mqttCallback(const mosquitto_message* message, void* context)
     string payload = string((char*) message->payload);
     printf("> %s\n> %s", topic.c_str(), payload.c_str());
     return 0;
+}
+
+void *tester(void* context)
+{
+    MqttClient* mqttClient = (MqttClient *) context;
+    string str;
+    while(true)
+    {
+        cin >>str;
+        if(str == ".q")
+            break;
+        else
+        {
+            char topic[] = "/cc3200/";
+            int ret = mqttClient->publish(NULL, topic, (int) str.length(), str.c_str(), 1, false);
+        }
+    }
+    return NULL;
 }
