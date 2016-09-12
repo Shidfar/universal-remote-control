ba3210c9f0dc76f2881516ea8c6748f0c618aa0b
Shidfar Hodizoda <shidfarhf@gmail.com>
2016-09-12 21:30:26 +0800

    adding device node

---
+++ DeviceNode.h
@@ -0,0 +1,15 @@
+//
+// Created by Shidfar Hodizoda on 9/12/16.
+//
+
+#ifndef UNISERVER_DEVICENODE_H
+#define UNISERVER_DEVICENODE_H
+
+
+class DeviceNode
+{
+
+};
+
+
+#endif //UNISERVER_DEVICENODE_H
