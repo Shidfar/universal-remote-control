d195283fe8f55b64af9938de7107991579901cf0
Shidfar Hodizoda <shidfarhf@yahoo.com>
2016-09-12 22:49:46 +0800

    good night

--- DeviceNode.h
+++ DeviceNode.h
@@ -5,10 +5,16 @@
 #ifndef UNISERVER_DEVICENODE_H
 #define UNISERVER_DEVICENODE_H
 
+#include "MqttClient.h"
 
 class DeviceNode
 {
+public:
+    string name;
+    MqttClient* mqttClient;
 
+    DeviceNode();
+    void set_mqttclient(MqttClient *_mqttClient);
 };
 
 
