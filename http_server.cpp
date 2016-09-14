e61e692295ab7af112df0d30505d0894c7affea4
Shidfar Hodizoda <shidfar@tractive.com.my>
2016-09-14 18:53:41 +0800

    pin up

--- http_server.cpp
+++ http_server.cpp
@@ -10,7 +10,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 
+#define FILENAME "picture.png"
+#define MIMETYPE "image/png"
 #define PORT 8888
 
 using namespace std;
@@ -22,30 +36,76 @@ int print_out_key (void *cls, enum MHD_ValueKind kind,
     return MHD_YES;
 }
 
-static int on_client_connect (void *cls, const struct sockaddr *addr, socklen_t addrlen);
+//static int on_client_connect (void *cls, const struct sockaddr *addr, socklen_t addrlen);
 
 int answer_to_connection (void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls)
 {
-    const char *page  = "<html><body>Hello, browser!</body></html>";
+    unsigned char *buffer = NULL;
     struct MHD_Response *response;
+    int fd;
     int ret;
+    struct stat sbuf;
 
-    response = MHD_create_response_from_buffer (strlen (page), (void*) page, MHD_RESPMEM_PERSISTENT);
+    if (0 != strcmp (method, "GET"))
+        return MHD_NO;
+    if ( (-1 == (fd = open (FILENAME, O_RDONLY))) || (0 != fstat (fd, &sbuf)) )
+    {
+        /* error accessing file */
+        if (fd != -1) close (fd);
+        const char *errorstr =
+                "<html><body>An internal server error has occured! </body></html>";
+        response = MHD_create_response_from_buffer (strlen (errorstr), (void *) errorstr, MHD_RESPMEM_PERSISTENT);
+        if (response)
+        {
+            ret = MHD_queue_response (connection, MHD_HTTP_INTERNAL_SERVER_ERROR, response);
+            MHD_destroy_response (response);
+            return MHD_YES;
+        }
+        else
+            return MHD_NO;
+        if (!ret)
+        {
+            const char *errorstr = "<html><body>An internal server error has occured!\
+                              </body></html>";
+
+            if (buffer) free(buffer);
+
+            response = MHD_create_response_from_buffer (strlen(errorstr), (void*) errorstr,
+                                                        MHD_RESPMEM_PERSISTENT);
+
+            if (response)
+            {
+                ret = MHD_queue_response (connection,
+                                          MHD_HTTP_INTERNAL_SERVER_ERROR,
+                                          response);
+                MHD_destroy_response (response);
+
+                return MHD_YES;
+            }
+            else return MHD_NO;
+        }
+    }
+    cout<<"access" <<endl;
+
+    response = MHD_create_response_from_fd_at_offset (sbuf.st_size, fd, 0);
+    MHD_add_response_header (response, "Refresh: 2; url=http://localhost:8888/", MIMETYPE);
+//    MHD_add_response_header (response, MHD_HTTP_HEADER_CONTENT_TYPE, MIMETYPE);
+
+    response = MHD_create_response_from_buffer(strlen("hey there"), (void*)"hey there", MHD_RESPMEM_PERSISTENT);
+
     ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
     MHD_destroy_response (response);
 
-    printf ("New %s request for %s using version %s\n", method, url, version);
-
-    MHD_get_connection_values (connection, MHD_HEADER_KIND, &print_out_key, NULL);
-
-    return MHD_NO;
+    return ret;
 }
 
 int main ()
 {
     struct MHD_Daemon *daemon;
 
-    daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
+//    daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
+//                               &answer_to_connection, NULL, MHD_OPTION_END);
+    daemon = MHD_start_daemon (MHD_USE_THREAD_PER_CONNECTION, PORT, NULL, NULL,
                                &answer_to_connection, NULL, MHD_OPTION_END);
     if (NULL == daemon) return 1;
     while (true)
@@ -59,6 +119,64 @@ int main ()
     MHD_stop_daemon (daemon);
     return 0;
 }
+
+//#include <sys/types.h>
+//#include <sys/select.h>
+//#include <sys/socket.h>
+//#include <microhttpd.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <iostream>
+//
+//#define PORT 8888
+//
+//using namespace std;
+//
+//int print_out_key (void *cls, enum MHD_ValueKind kind,
+//                   const char *key, const char *value)
+//{
+//    printf ("%s: %s\n", key, value);
+//    return MHD_YES;
+//}
+//
+//static int on_client_connect (void *cls, const struct sockaddr *addr, socklen_t addrlen);
+//
+//int answer_to_connection (void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls)
+//{
+//    const char *page  = "<html><body>Hello, browser!</body></html>";
+//    struct MHD_Response *response;
+//    int ret;
+//
+//    response = MHD_create_response_from_buffer (strlen (page), (void*) page, MHD_RESPMEM_PERSISTENT);
+//    ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+//    MHD_destroy_response (response);
+//
+//    printf ("New %s request for %s using version %s\n", method, url, version);
+//
+//    MHD_get_connection_values (connection, MHD_HEADER_KIND, &print_out_key, NULL);
+//
+//    return MHD_NO;
+//}
+//
+//int main ()
+//{
+//    struct MHD_Daemon *daemon;
+//
+//    daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
+//                               &answer_to_connection, NULL, MHD_OPTION_END);
+//    if (NULL == daemon) return 1;
+//    while (true)
+//    {
+//        char c;
+//        cin>>c;
+//        if(c == 'q')
+//            break;
+//    }
+//
+//    MHD_stop_daemon (daemon);
+//    return 0;
+//}
 
 
 
